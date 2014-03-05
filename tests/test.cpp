344431a6fb92a9ce86f9bda6d17e20ae3d758aaf
Alvaro Frank <alvarofrank@gmail.com>
2014-03-05 19:54:09 +0000

    Added base tests for validity under tests folder. This should be passed by any build of the solver.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/genabel/pkg/OmicABELnoMM@1635 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ tests/test.cpp
@@ -0,0 +1,85 @@
+//export MKL_NUM_THREADS=1 OMP_NUM_THREADS=1 OMP_NESTED=true
+#include <unistd.h>
+#include <getopt.h>
+
+
+
+#include "../src/Definitions.h"
+#include "../src/Algorithm.h"
+
+
+
+int main(int argc, char *argv[] )
+{
+    struct Settings params;
+
+    params.ForceCheck = true;
+
+
+    //!default params
+    params.r = 1;
+    params.threads = 1;
+
+    omp_set_num_threads(params.threads);
+    blas_set_num_threads(params.threads);
+
+    Algorithm alg;
+
+    params.use_fake_files = true;
+    int iters = 10;
+    int max_threads = 2;
+
+
+    for (int th = 0; th < max_threads; th++)
+    {
+        params.threads = th;
+
+        params.n=10; params.l=4;  params.r=1;
+        params.t=16; params.tb=1; params.m=16; params.mb=1;
+
+        struct Outputs out = {0};
+        for (int i = 0; i < iters; i++)
+        {
+            alg.solve(params, out, P_NEQ_B_OPT_MD);
+        }
+        cout << endl;
+        /******************************/
+        params.n=10; params.l=4;  params.r=2;
+        params.t=16; params.tb=4; params.m=16; params.mb=4;
+
+
+        for (int i = 0; i < iters; i++)
+        {
+            alg.solve(params, out, P_NEQ_B_OPT_MD);
+        }
+        cout << endl;
+        /******************************/
+        params.n=10; params.l=4;  params.r=2;
+        params.t=16; params.tb=5; params.m=16; params.mb=3;
+
+
+        for (int i = 0; i < iters; i++)
+        {
+            alg.solve(params, out, P_NEQ_B_OPT_MD);
+        }
+        cout << endl;
+        /******************************/
+        params.n=10; params.l=4;  params.r=2;
+        params.t=4; params.tb=4; params.m=4; params.mb=4;
+
+
+        for (int i = 0; i < iters; i++)
+        {
+            alg.solve(params, out, P_NEQ_B_OPT_MD);
+        }
+        cout << endl;
+
+    }
+
+    cout << "\nTest finished succesfully\n";
+
+
+
+
+    return 0;
+}
